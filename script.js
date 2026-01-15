a1f1032a67875e06e0cdb5e47692ac7666a8b964
SafatSadik <154873148+SafatSadik@users.noreply.github.com>
2026-01-16 00:08:43 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -3,48 +3,24 @@ const app = express();
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
-// app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-let users = {};
 
 io.on("connection", (socket) => {
-    const userId = socket.id;
-    users[userId] = { id: userId, color: getRandomColor() };
-    // io.emit("updateUsers", users);
-    // io.emit("userJoined", `User ${userId}`);
-
-    // console.log(userId)
-
     socket.on("start", (data) => {
         io.emit("start", data);
     });
-
     socket.on("draw", (data) => {
         socket.broadcast.emit("draw", data);
     });
-
     socket.on("msg", (msg) => {
         socket.broadcast.emit("msg", msg); 
     });
-
-
     socket.on("clear", () => {
         socket.broadcast.emit("clear");
     });
-    socket.on("message", (data) => {
-        console.log(data)
-    })
-
-    socket.on("disconnect", () => {
-        io.emit("userLeft", `User ${userId}`);
-        delete users[userId];
-        io.emit("updateUsers", users);
-    });
 });
 
-function getRandomColor() {
-    return `hsl(${Math.random() * 360}, 100%, 50%)`;
-}
 
-server.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
